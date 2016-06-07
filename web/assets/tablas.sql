10023834be82e49501d049401242cae73db018b4
Administrador <Administrador@localhost>
2016-06-06 21:07:38 -0400

    Sql para la base de datos

---
+++ web/assets/tablas.sql
@@ -0,0 +1,32 @@
+create table users (
+	user_id int(10) primary key auto_increment,
+	user varchar(20),
+	password varchar(20),
+	birth_date date,
+	status tinyint(1)
+);
+
+create table books (
+	book_id int(10) primary key auto_increment,
+	name varchar(50),
+	isbn varchar(20),
+	pages int(4),
+	author_id int(10),
+	state tinyint(1),
+	created_by int(10),
+	publish_date date
+);
+
+create table authors (
+	author_id int(10) primary key auto_increment,
+	name varchar(40),
+	apepat varchar(30),
+	apemat varchar(30),
+	birth_date date,
+);
+
+create table nationalities (
+	nation_id int(10) primary key auto_increment,
+	name varchar(20),
+	state tinyint(1)
+);
